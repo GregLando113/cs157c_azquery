bda3744ea61a671fbe25e6582dd6d256bfc0e6be
Greg <greglando@gmail.com>
2022-07-31 14:13:55 -0700

    add first asin route

--- azquery/cli.py
+++ azquery/cli.py
@@ -2,11 +2,25 @@ import click
 import pymongo
 
 
+conn = pymongo.MongoClient('mongodb://44.193.181.226:27018')
+db = conn.get_database('testdb')
 
-
-
+@click.group()
 def main():
     pass
+
+@main.command()
+@click.argument('asin')
+def product_reviews(asin):
+    '''Return product reviews for a specific product by its amazon identifier (asin).'''
+    reviews = db.get_collection('reviews')
+    cur = reviews.find({'asin': asin})
+    for res in cur:
+        print(res)
+
+# @main.command()
+# def inspect_reviewer()
+
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ setup(
     name='Amazon Review Reviewer',
     version='0.1',
     packages=['azquery'],
+    include_package_data=True,
     entry_points={
         'console_scripts': [
-        'azquery = azquery.cli:main',
+            'azquery = azquery.cli:main',
         ],
     },
     install_requires=[
